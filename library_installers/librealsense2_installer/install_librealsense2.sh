ded55f31fa5a002f5770c297be2e848cc1f2a5cd
Sakurai Ryuhei <sakurai@chitose-robotics.com>
2024-09-03 18:48:13 +0900

    Add test code to check mvviewer　interfere with librealsense2

---
+++ library_installers/librealsense2_installer/install_librealsense2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+sudo apt-get install -y apt-transport-https
+echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
+sudo tee /etc/apt/sources.list.d/librealsense.list
+sudo apt update
+
+sudo apt install -y librealsense2-dev librealsense2-utils
