1b36332f22d1963333477cd3ba2698d5185962e0
Sakurai Ryuhei <sakurai@chitose-robotics.com>
2024-09-27 15:06:25 +0900

    Attempt to remove shell script option -e to prevent the installation failure

--- library_installers/mvviewer_installer/install_mvviewer.sh
+++ library_installers/mvviewer_installer/install_mvviewer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -x
 
 installer_file_name=MVviewer_Ver2.4.2.9_Linux_x86_Build20240923.run
 wget -P /tmp https://github.com/sakurai-ryuhei/reproduce-interference-between-librealsense2-and-mvviewer/releases/download/v0.0.0/$installer_file_name
